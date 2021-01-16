3c96b9085d5aee715cdb705b2f8f03df4492f0a8
IsaiahWitzke <isaiah.witzke@gmail.com>
2021-01-16 18:33:45 -0500

    docker changes

--- backend/routes/caption.js
+++ backend/routes/caption.js
@@ -5,7 +5,7 @@ const { getCaption } = require("../db/caption");
 router.post("/", async (req, res) => {
     // parse req
   const googleVisionFoundWords = req.body.words;
-  console.log(googleVisionFoundWords);
+  console.log(req);
   // map words input to actual keywords (use synonyms)
   const caption = await getCaption(googleVisionFoundWords);
   res.send(caption);
