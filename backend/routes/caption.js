858aec6f947c0d51dde5da4374892d2f75f2b49d
Betty Guo <bettyg3113@gmail.com>
2021-01-16 19:52:07 -0500

    return type json

--- backend/routes/caption.js
+++ backend/routes/caption.js
@@ -6,7 +6,6 @@ router.post("/", async (req, res) => {
     // parse req
   const googleVisionFoundWords = req.body.words;
   console.log(req);
-  // map words input to actual keywords (use synonyms)
   const caption = await getCaption(googleVisionFoundWords);
   res.send(caption);
 });
